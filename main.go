970f4ec4988a3f932fbc884614a46d3d04f15e1e
jantb <jan@boee.org>
2016-09-30 20:53:38 +0200

    1s poll

--- main.go
+++ main.go
@@ -94,7 +94,7 @@ func main() {
 	edit_box.quitSearch = make(chan bool)
 	go func() {
 		for {
-			time.Sleep(time.Millisecond * 100)
+			time.Sleep(time.Millisecond * 1000)
 			if edit_box.seek == int64(0) {
 				edit_box.Search()
 				redraw_all()
