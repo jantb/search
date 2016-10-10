e49874d0c9158972d87e7e77014630e0939c8042
jantb <jan@boee.org>
2016-10-10 20:18:19 +0200

    Changed so we dont deselect all when using not

--- tail/tail.go
+++ tail/tail.go
@@ -1,24 +1,26 @@
 package tail
 
 import (
-	"time"
-	"os"
-	"github.com/hpcloud/tail"
-	"log"
 	"encoding/binary"
 	"github.com/boltdb/bolt"
+	"github.com/hpcloud/tail"
 	"github.com/jantb/search/proto"
+	"log"
+	"os"
 	"path/filepath"
 	"sync"
+	"time"
 )
+
 var regenChan = make(chan []byte, 10000)
 var once sync.Once
+
 func regenerateBloom(keys chan []byte, db *bolt.DB) {
 	for {
 		k := <-keys
 		err := db.Update(func(tx *bolt.Tx) error {
 			b := tx.Bucket([]byte("Events"))
-			b,_ = b.CreateBucketIfNotExists(Int64timeToByte(ByteToint64timeTo(k).Truncate(24 * time.Hour).Unix()))
+			b, _ = b.CreateBucketIfNotExists(Int64timeToByte(ByteToint64timeTo(k).Truncate(24 * time.Hour).Unix()))
 
 			by := b.Get(k)
 			var e proto.Events
@@ -28,7 +30,6 @@ func regenerateBloom(keys chan []byte, db *bolt.DB) {
 			}
 			if e.BloomDirty {
 				e.RegenerateBloom()
-
 
 				by, err = e.Marshal()
 				if err != nil {
@@ -46,10 +47,10 @@ func regenerateBloom(keys chan []byte, db *bolt.DB) {
 
 func tailFile(fileMonitor proto.FileMonitor, db *bolt.DB) {
 	t, err := tail.TailFile(fileMonitor.Path, tail.Config{Follow: true,
-		ReOpen:true,
-		Poll: fileMonitor.Poll,
-		Logger:tail.DiscardingLogger,
-		Location:&tail.SeekInfo{fileMonitor.Offset, os.SEEK_SET}})
+		ReOpen:   true,
+		Poll:     fileMonitor.Poll,
+		Logger:   tail.DiscardingLogger,
+		Location: &tail.SeekInfo{fileMonitor.Offset, os.SEEK_SET}})
 	var key []byte
 	var dayKey []byte
 	var prevData string
@@ -77,11 +78,11 @@ func tailFile(fileMonitor proto.FileMonitor, db *bolt.DB) {
 			if ok == 0 {
 				tt = ti
 				stopo = prevo
-				if tt.Before(time.Now().Truncate(time.Hour * 24).AddDate(0,-1,0)) {
+				if tt.Before(time.Now().Truncate(time.Hour*24).AddDate(0, -1, 0)) {
 					key = nil
 					continue
 				}
-				text = text[len(f) + 1:]
+				text = text[len(f)+1:]
 				ok = 1
 			}
 		}
@@ -91,13 +92,13 @@ func tailFile(fileMonitor proto.FileMonitor, db *bolt.DB) {
 		}
 		prevo = o
 		// Multiline entry add to last timestamp
-		if (ok == -1 || ok == 0 ){
+		if ok == -1 || ok == 0 {
 			if key == nil {
 				continue
 			}
 			err = db.Update(func(tx *bolt.Tx) error {
 				b := tx.Bucket([]byte("Events"))
-				b,_ = b.CreateBucketIfNotExists(dayKey)
+				b, _ = b.CreateBucketIfNotExists(dayKey)
 				by := b.Get(key)
 
 				var events proto.Events
@@ -127,10 +128,10 @@ func tailFile(fileMonitor proto.FileMonitor, db *bolt.DB) {
 		}
 
 		var event = proto.Event{
-			Ts: tt.Format(time.RFC3339),
-			Data: text,
-			Path: fileMonitor.Path,
-			BloomDirty:true,
+			Ts:         tt.Format(time.RFC3339),
+			Data:       text,
+			Path:       fileMonitor.Path,
+			BloomDirty: true,
 		}
 
 		key = Int64timeToByte(tt.Truncate(1 * time.Minute).Unix())
@@ -139,7 +140,7 @@ func tailFile(fileMonitor proto.FileMonitor, db *bolt.DB) {
 		prevTs = event.Ts
 		err = db.Update(func(tx *bolt.Tx) error {
 			b := tx.Bucket([]byte("Events"))
-			b,_ = b.CreateBucketIfNotExists(dayKey)
+			b, _ = b.CreateBucketIfNotExists(dayKey)
 			eventsb := b.Get(key)
 			if eventsb == nil {
 				var events proto.Events
@@ -210,6 +211,12 @@ func findFormat(text string) string {
 
 var formats = []string{"2006/01/02 15:04:05",
 	"2006-01-02 15:04:05.000",
+	"2006-01-02 15:04:05",
+	"2006-01-02 15:04:05.0",
+	"2006-01-02 15:04:05.00",
+	"2006-01-02 15:04:05.0000",
+	"2006-01-02 15:04:05.00000",
+	"2006-01-02 15:04:05.000000",
 	time.ANSIC,
 	time.UnixDate,
 	time.RubyDate,
@@ -229,9 +236,8 @@ func Int64timeToByte(i int64) []byte {
 }
 
 func ByteToint64timeTo(bytes []byte) time.Time {
-	return time.Unix(int64(binary.BigEndian.Uint64(bytes)),int64(0))
+	return time.Unix(int64(binary.BigEndian.Uint64(bytes)), int64(0))
 }
-
 
 func TailAllFiles(db *bolt.DB) {
 	once.Do(func() {
@@ -268,9 +274,9 @@ func AddFileToTail(filename string, poll bool, db *bolt.DB) {
 			dir, _ := filepath.Abs(filepath.Dir(filename))
 			filep := filepath.Join(dir, filepath.Base(filename))
 			fileMonitor := proto.FileMonitor{
-				Path:filep,
-				Offset:0,
-				Poll: poll,
+				Path:   filep,
+				Offset: 0,
+				Poll:   poll,
 			}
 			by, err := fileMonitor.Marshal()
 			if err != nil {
@@ -282,4 +288,3 @@ func AddFileToTail(filename string, poll bool, db *bolt.DB) {
 		return
 	}
 }
-
