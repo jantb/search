79f00ca41bb3414136646dbe6927d359cc53934c
jantb <jan@boee.org>
2017-04-25 06:13:11 +0200

    speed bump

--- proto/events_test.go
+++ proto/events_test.go
@@ -11,7 +11,7 @@ func TestEvent_Search_match_not_match(t *testing.T) {
 	e := Event{
 		BloomDirty: true,}
 	e.SetData(dataText)
-	e.GenerateBloom()
+	e.BloomUpdate()
 	if !e.ShouldAddAndGetIndexes([]string{"support"}) {
 		t.Fail()
 	}
@@ -25,7 +25,7 @@ func TestEvent_Search_field(t *testing.T) {
 		BloomDirty: true,}
 
 	e.SetData(dataText)
-	e.GenerateBloom()
+	e.BloomUpdate()
 	if !e.ShouldAddAndGetIndexes([]string{"hello>2"}) {
 		t.Fail()
 	}
