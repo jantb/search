6d00b87ed788f9c72f45ee219244e15655bbe3ff
jantb <jan@boee.org>
2021-09-17 20:51:50 +0200

    Support kafka with ctrl k

--- go.mod
+++ go.mod
@@ -4,13 +4,14 @@ go 1.12
 
 require (
 	github.com/Shopify/sarama v1.29.1
+	github.com/TylerBrock/colorjson v0.0.0-20200706003622-8a50f05110d2
 	github.com/fatih/color v1.7.0
+	github.com/hokaccha/go-prettyjson v0.0.0-20210113012101-fb4e108d2519 // indirect
 	github.com/jroimartin/gocui v0.4.0
 	github.com/mattn/go-colorable v0.1.4 // indirect
 	github.com/mattn/go-isatty v0.0.10 // indirect
 	github.com/mattn/go-runewidth v0.0.6 // indirect
 	github.com/nsf/termbox-go v0.0.0-20190817171036-93860e161317 // indirect
-	github.com/segmentio/kafka-go v0.4.18
 	github.com/valyala/fastjson v1.6.1
 	go.uber.org/atomic v1.5.1
 	go4.org/intern v0.0.0-20210108033219-3eb7198706b2
--- kafka/kafka.go
+++ kafka/kafka.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/Shopify/sarama"
+	"github.com/TylerBrock/colorjson"
 	"github.com/jantb/search/logline"
 	"log"
 	"os"
@@ -19,21 +20,26 @@ func KafkaRead(insertLogLinesChan chan logline.LogLine, quit chan bool) {
 		cons = []string{"localhost:9092"}
 	}
 
-	consumer, err := sarama.NewConsumer(cons, sarama.NewConfig())
+	config := sarama.NewConfig()
+	config.ClientID = "t"
+	config.Consumer.Offsets.AutoCommit.Enable = false
+	config.Consumer.Offsets.Initial = sarama.OffsetOldest
+	consumer, err := sarama.NewConsumer(cons, config)
 	if err != nil {
-
+		log.Println(err)
 	}
 	//defer consumer.Close()
 
-	strings, err := consumer.Topics()
-	for _, topic := range strings {
+	topics, err := consumer.Topics()
+	for _, topic := range topics {
 		partitions, err := consumer.Partitions(topic)
 		if err != nil {
-
+			log.Println(topic)
+			log.Println(err)
+			continue
 		}
 		var consumers []sarama.PartitionConsumer
 		for _, partition := range partitions {
-
 			consumePartition, err := consumer.ConsumePartition(topic, partition, sarama.OffsetOldest)
 			//defer consumePartition.Close()
 			if err != nil {
@@ -51,7 +57,16 @@ func KafkaRead(insertLogLinesChan chan logline.LogLine, quit chan bool) {
 					}
 					l.SetSystem(fmt.Sprintf("%s %d %d", topic, partition, message.Offset))
 					l.SetLevel(string(message.Key))
-					indent, err := json.MarshalIndent(string(message.Value), "", "    ")
+
+					var obj map[string]interface{}
+					err := json.Unmarshal(message.Value, &obj)
+					if err != nil {
+						l.SetBody(string(message.Value))
+						insertLogLinesChan <- l
+						continue
+					}
+
+					indent, err := colorjson.Marshal(obj)
 					if err != nil {
 						l.SetBody(string(message.Value))
 					} else {
